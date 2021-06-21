e1eaa5139913eba1d01ee1713bc5acaad8e91783
Logeswaran A <lokesh@troondx.com>
2021-06-22 00:13:26 +0530

    Added VinterAdapter

---
+++ VinterAPI_Adapter/index.js
@@ -0,0 +1,48 @@
+const { Requester, Validator } = require('@chainlink/external-adapter')
+
+const customError = (data) => {
+  if (data.Response === 'Error') return true
+  return false
+}
+
+const customParams = {
+  symbol: ['symbol']
+}
+
+const createRequest = (input, callback) => {
+  console.log("invput ",input)
+  const validator = new Validator(callback, input, customParams)
+  const jobRunID = validator.validated.id
+  const endpoint = validator.validated.endpoint || 'single_assets_daily'
+  const url = `https://www.vinterapi.com/api/v2/${endpoint}`
+  const symbol = 'xdc-usd-p-d'
+
+  const params = {
+    symbol
+  }
+
+  const config = {
+    url,
+    params
+  }
+
+  // if (process.env.API_KEY) {
+    config.headers = {
+      Authorization: 'zleorhzf.8pXiPL1ZY5FPbaWeE9WeYIY9mudGPHna'
+    }
+
+  // }
+
+  Requester.request(config, customError)
+    .then(response => {
+      console.log("response.data",response.data)
+
+      response.data.result = Requester.validateResultNumber(response.data,['value']);
+      callback(response.status, Requester.success(jobRunID, response))
+    })
+    .catch(error => {
+      callback(500, Requester.errored(jobRunID, error))
+    })
+}
+
+module.exports.createRequest = createRequest
